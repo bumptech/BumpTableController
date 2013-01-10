69d46f938e99ff04ed6321b3a7c280a9470e5be6
Sahil Desai <sahil@sahild.com>
2013-01-10 14:36:41 -0800

    fix compile issue

--- BumpTableView.h
+++ BumpTableView.h
@@ -20,12 +20,12 @@
 
 @property (nonatomic) BOOL showSectionIndexTitles;                          // show scrubber. Default is NO
 @property (nonatomic) BOOL allowsSwipeConfirmation;                         // show button over cell when user swipes. Default is NO 
-@property (nonatomic) NSString *swipeConfirmationTitle;                     // title of swipe button. Default is "Delete"
+@property (nonatomic, strong) NSString *swipeConfirmationTitle;                     // title of swipe button. Default is "Delete"
 
-@property (nonatomic) UISearchBar *searchBar;                               // upon first access, the search bar is added to the table header
+@property (nonatomic, strong) UISearchBar *searchBar;                               // upon first access, the search bar is added to the table header
 
 // Model changes
-@property (nonatomic) BumpTableModel *model;                                // setting a model will automatically call reload the data using the new model (not animated)
+@property (nonatomic, strong) BumpTableModel *model;                                // setting a model will automatically call reload the data using the new model (not animated)
 - (void)transitionToModel:(BumpTableModel *)newModel;                       // animated version of setModel: using UITableViewRowAnimationTop for all row insertions/deletions
 
 @property (nonatomic) UITableViewRowAnimation transtionAnimation;           // animation transition to use for row insertions/deletions (does not affect move animations)
